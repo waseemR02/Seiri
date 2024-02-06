5a2f57fa1197ebaa98ed21930a77aa057f559fac
waseemR02 <waseem.riaz.9999@gmail.com>
2024-02-06 19:36:34 +0530

    Add tests for validate.py

---
+++ tests/test_validate.py
@@ -0,0 +1,71 @@
+from seiri.validate import Validate
+
+
+def test_validate_correct(tmp_path):
+    # Create a temporary input file path
+    input_file = "tests/Delivered_correct.xlsx"
+
+    # Create a temporary against file path
+    against_file = "tests/Sample.xlsx"
+
+    # Create a temporary log file path
+    log_file = tmp_path / "seiri-error.log"
+
+    # Create an instance of Validate
+    validator = Validate(str(log_file))
+
+    # Call the validate method with correct xlsx files
+    result = validator.validate(str(input_file), str(against_file))
+
+    # Check if the validation is successful
+    assert result is True
+
+    # Check if the log file is created
+    assert log_file.exists()
+
+
+def test_validate_incorrect(tmp_path):
+    # Create a temporary input file path
+    input_file = "tests/Delivered_incorrect.xlsx"
+
+    # Create a temporary against file path
+    against_file = "tests/Sample.xlsx"
+
+    # Create a temporary log file path
+    log_file = tmp_path / "seiri-error.log"
+
+    # Create an instance of Validate
+    validator = Validate(str(log_file))
+
+    # Call the validate method with correct xlsx files
+    result = validator.validate(str(input_file), str(against_file))
+
+    # Check if the validation is successful
+    assert result is False
+
+    # Check if the log file is created
+    assert log_file.exists()
+
+    # Check if the contents are similar to the expected log file in tests/sample_log_validate.log
+    # But ignore the date and time
+    # Sample
+    # | ERROR    | Validate | Row count mismatch in de sheet
+    # | ERROR    | Validate | Mismatched value in it sheet at row 6. Expected: lbl_Reserved, Actual: label_Expert_List
+    # | ERROR    | Validate | Empty cell in de sheet at row 7
+    # | ERROR    | Validate | Mismatched value in it sheet at row 7. Expected: label_Expert_List, Actual: lbl_Reserved
+    # | ERROR    | Validate | Duplicate value 'df' found in de sheet
+
+    with open(log_file, "r") as f:
+        log_contents = f.readlines()
+        assert len(log_contents) == 5
+        assert "Row count mismatch in de sheet" in log_contents[0]
+        assert (
+            "Mismatched value in it sheet at row 6. Expected: lbl_Reserved, Actual: label_Expert_List"
+            in log_contents[1]
+        )
+        assert "Empty cell in de sheet at row 7" in log_contents[2]
+        assert (
+            "Mismatched value in it sheet at row 7. Expected: label_Expert_List, Actual: lbl_Reserved"
+            in log_contents[3]
+        )
+        assert "Duplicate value 'df' found in de sheet" in log_contents[4]
